11a196417b0a6adf7cc033f1d077a8ce28353acd
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-11 15:10:29 -0400

    creating components folder and adding footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import Footer from './compenents/Footer';
 
 function App() {
+
+
+  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  // const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      {/* <Nav
+        // categories={categories}
+        // setCurrentCategory={setCurrentCategory}
+        // currentCategory={currentCategory}
+        // contactSelected={contactSelected}
+        // setContactSelected={setContactSelected}
+      ></Nav> */}
+      <main>
+        {/* {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )} */}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
