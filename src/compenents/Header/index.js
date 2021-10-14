ce350a413069a33aea4931ce71acb16149ec3f9e
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-13 20:33:47 -0400

    navigation component

--- src/compenents/Header/index.js
+++ src/compenents/Header/index.js
@@ -17,38 +17,7 @@ function Header(props) {
                     Lernantino
                 </a>
             </h2>
-            <nav>
-
-                <NavLink className="mx-4" activeClassName="active" to="/aboutme">
-                    About me
-                </NavLink>
-                <NavLink className="mx-4" activeClassName="active" to="/portfolio">
-                    Portfolio
-                </NavLink>
-                <NavLink className="mx-4" activeClassName="active" to="/contact">
-                    Contact
-                </NavLink>
-                <NavLink className="mx-4" activeClassName="active" to="/resume">
-                    Resume
-                </NavLink>
-                {/* <ul className="flex-row">
-                    <li className="mx-4">
-                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-                            About me
-                        </a>
-                    </li>
-                    <li className={`mx-4 ${contactSelected && 'navActive'}`}>
-                        <span onClick={() => setContactSelected(false)}>Portfolio</span>
-                    </li>
-                    <li className={`mx-4 ${contactSelected && 'navActive'}`}>
-                        <span onClick={() => setContactSelected(true)}>Contact</span>
-                    </li>
-                    <li className={`mx-4 ${contactSelected && 'navActive'}`}>
-                        <span onClick={() => setContactSelected(false)}>Resume</span>
-                    </li>
-
-                </ul> */}
-            </nav>
+            <Nav></Nav>
         </header>
     );
 }
--- src/compenents/Navigation/index.js
+++ src/compenents/Navigation/index.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from 'react';
 // import { capitalizeFirstLetter } from '../../utils/helpers';
+import { NavLink } from "react-router-dom";
 
 function Nav() {
 
 
   return (
 
-    <div></div>
+    <nav>
+
+      <NavLink className="mx-4" activeClassName="active" to="/aboutme">
+        About me
+      </NavLink>
+      <NavLink className="mx-4" activeClassName="active" to="/portfolio">
+        Portfolio
+      </NavLink>
+      <NavLink className="mx-4" activeClassName="active" to="/contact">
+        Contact
+      </NavLink>
+      <NavLink className="mx-4" activeClassName="active" to="/resume">
+        Resume
+      </NavLink>
+    </nav>
 
   );
 }
